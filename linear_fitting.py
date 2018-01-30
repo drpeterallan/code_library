8d23245e3ab5244f7d4db60a43960562eaa903dd
Peter Allan <phy3pa@gmail.com>
2018-01-30 20:02:28 +0000

    Added fitting errors and plot formatting

--- linear_fitting.py
+++ linear_fitting.py
@@ -1,5 +1,5 @@
 from __future__ import division, print_function  # python 2 to 3 compatibility
-from numpy import linspace, polyfit, polyval
+from numpy import linspace, polyfit, polyval, sqrt, diag
 from random import random
 import matplotlib.pyplot as plt
 
@@ -16,13 +16,21 @@ def polynomial_fitting():
     x_fit = linspace(0, 10, 10)
     y_fit = polyval(coefs, x_fit)
 
-    # Plot the data and the fit
-    plt.plot(x_array, y_array, "bo")
-    plt.plot(x_fit, y_fit, "r-", lw=2)
+    # Compute fitting errors
+    gradient, intercept = coefs
+    gradient_error, intercept_error = sqrt(diag(cov))
 
-    # Plot formatting
+    # Plot the data and the fit
+    plt.plot(x_array, y_array, "bo", label="Data")
+    plt.plot(x_fit, y_fit, "r-", lw=2, label="Fit")
+
+    # Plot formatting etc.
     plt.xlabel("x", fontsize=16)
     plt.ylabel("y", fontsize=16)
+    plt.legend(loc="upper left")
+    plt.title("m = " + str(round(gradient, 3)) + " +/- " + str(round(gradient_error, 3))
+              + "\n"
+              + "c = " + str(round(intercept, 3)) + " +/- " + str(round(intercept_error, 3)))
     plt.show()
 
 
