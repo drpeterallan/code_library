1228cb5b34a2a2b75b33766407dec321147736ba
Peter Allan <phy3pa@gmail.com>
2018-03-02 12:57:52 +0000

    Extract and plot errors from fit

--- read_streak_image.py
+++ read_streak_image.py
@@ -1,14 +1,12 @@
 from __future__ import division, print_function  # python 2 to 3 compatibility
 import matplotlib.pyplot as plt
-from numpy import shape, array, exp, linspace, arange
+from numpy import shape, array, exp, arange, diag, sqrt, append, zeros
 from my_functions.pa3 import search_function
 from scipy.optimize import curve_fit
 
 
 def read_image_file(path_to_file):
-
     # Function to read image file and return in format you would get real image
-
     # Read in image file and convert to a numpy array
     pixel_vals = array(plt.imread(path_to_file), dtype=float)
     pixel_vals = pixel_vals[::-1, :]  # Correct for reversal of data in y axis
@@ -23,16 +21,13 @@ def read_image_file(path_to_file):
 
 
 def contour_image_plot(x_axis, y_axis, pixel_vals):
-
     # Function to plot data
-
     plt.contourf(x_axis, y_axis, pixel_vals, 50)
     plt.xlabel("Time")
     plt.ylabel("Space")
 
 
 def get_yaxis_lineout(y_axis, pixel_vals, y_lineout_position):
-
     # Function to get a lineout in the y axis
     # Search y_axis for user requested position
     y_index_pos, _ = search_function(y_axis, y_lineout_position)
@@ -47,9 +42,10 @@ def fit_edge_profile(x_array, y_array):
 
     # Find edge position
     coefs_int = [0.0, 1.0, 1.0, 1.0]  # Initial guess of parameters
-    coefs, _ = curve_fit(sigmoid_func, x_array, y_array, coefs_int)
+    coefs, cov = curve_fit(sigmoid_func, x_array, y_array, coefs_int)
+    errors = sqrt(diag(cov))
 
-    return coefs
+    return coefs, errors
 
 
 if __name__ == "__main__":
@@ -58,8 +54,10 @@ if __name__ == "__main__":
     image_location = "/home/peter/Documents/streak_image_analysis/streak_test_image.png"
     time, space, data = read_image_file(image_location)
 
-    lineout_positions = arange(200, 300)
-    time_positions = []
+    lineout_positions = arange(0, 450, 50)
+    edge_times = []
+    edge_times_errors = []
+
     for lineout_position in lineout_positions:
 
         # Pull a lineout from the image
@@ -67,8 +65,9 @@ if __name__ == "__main__":
         lineout_vals = get_yaxis_lineout(space, data, lineout_position)
 
         # Find the edge by fitting a sigmoid
-        coefs = fit_edge_profile(time, lineout_vals)
-        time_positions.append(coefs[2])
+        coefs, errors = fit_edge_profile(time, lineout_vals)
+        edge_times.append(coefs[2])
+        edge_times_errors.append(errors[2])
 
         # Plot data and fit
         # plt.plot(time, lineout_vals, "b-", lw=2)
@@ -77,10 +76,14 @@ if __name__ == "__main__":
         # plt.title(coefs[2])
         # plt.show()
 
+    edge_times = array(edge_times, dtype=float)
+    edge_times_errors = array(edge_times_errors, dtype=float) * 10
 
     # Plot the image
     contour_image_plot(time, space, data)
-    plt.plot(time_positions, lineout_positions, "r-", lw=2)
+    plt.errorbar(edge_times, lineout_positions, xerr=edge_times_errors, lw=0)
     plt.show()
 
+    plt.errorbar(edge_times, lineout_positions, fmt="o", xerr=edge_times_errors, lw=0)
+    plt.show()
 
