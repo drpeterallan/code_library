ad6bdfc0d5c3d891df84160661019e2a41e582c7
Peter Allan <phy3pa@gmail.com>
2018-04-07 10:29:31 +0100

    Fitting with an ellipse

--- circle_fitter.py
+++ circle_fitter.py
@@ -7,12 +7,22 @@ import matplotlib.pyplot as plt
 def circle_function(x_array, a, b, r):
     # Equation of a circle (x - a)^2 + (y - b)^2 = r^2
     # In cartesian coordinates this will return the +x/+y portion of the circle
-    return sqrt(-a**2 + (2 * a * x_array) + r**2 - x_array**2) + b**2
+    return (-a**2 + (2 * a * x_array) + r**2 - x_array**2)**0.5 + b**2
+
+
+def ellipse_function(x_array, a, b, x0, y0):
+    return ((b * (a**2 + x_array**2 + (2 * x_array * x0 - x0**2))**0.5) / a) + y0
 
 
 def fit_circle(x_array, y_array):
     coefs_int = [0.0, 0.0, 20]  # Initial guess of parameters
     coefs, _ = curve_fit(circle_function, x_array, y_array, coefs_int)
+    return coefs
+
+
+def fit_ellipse(x_array, y_array):
+    coefs_int = [1.0, 1.0, 0.0, 0.0]
+    coefs, _ = curve_fit(ellipse_function, x_array, y_array, coefs_int)
     return coefs
 
 
@@ -27,11 +37,12 @@ if __name__ == "__main__":
     plt.plot(x_data, y_data, "bo", label="data")
 
     # Perform fit to data
-    coefs = fit_circle(x_data, y_data)
+    coefs = fit_ellipse(x_data, y_data)
+    print(coefs)
 
     # Create the fit line
     x_fit = arange(min(x_data), max(x_data), 0.1)
-    y_fit = circle_function(x_fit, coefs[0], coefs[1], coefs[2])
+    y_fit = ellipse_function(x_fit, coefs[0], coefs[1], coefs[2], coefs[3])
 
     # Overplot fit
     plt.plot(x_fit, y_fit, "r-", lw=2, label="fit")
@@ -41,6 +52,6 @@ if __name__ == "__main__":
     plt.ylabel("y axis", fontsize=16)
     plt.tick_params(axis="both", labelsize=16, pad=5)
     plt.legend(loc="lower left")
-    plt.xlim(-1, 21)
-    plt.ylim(5, 22)
+    # plt.xlim(-1, 21)
+    # plt.ylim(5, 22)
     plt.show()
