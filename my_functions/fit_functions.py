ccdbb84a931ad5c4bb5dc437ef215d4d849a1950
Peter Allan <phy3pa@gmail.com>
2018-04-30 19:58:47 +0100

    Created a module for all common fitting funcitons

---
+++ my_functions/fit_functions.py
@@ -0,0 +1,11 @@
+from __future__ import division, print_function  # python 2 to 3 compatibility
+from numpy import sqrt, log, exp
+
+
+def linear_function(x_array, m, c):
+    return m * x_array + c
+
+
+def gaussian_function(x_array, height, centre, fwhm):
+    c = fwhm / (2 * sqrt(2 * log(2)))
+    return height * exp(-(x_array - centre)**2 / (2.0 * c**2.0))
