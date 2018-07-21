a573b7bb6b81cb7c0ebad10fae50d56db18526a7
Peter Allan <phy3pa@gmail.com>
2018-07-21 06:50:51 +0100

    Comments/formatting. gitignore file test

--- plot_flychk_output.py
+++ plot_flychk_output.py
@@ -24,22 +24,27 @@ if __name__ == "__main__":
     energy_interp = linspace(min(energy), max(energy), 5e3)
     intensity_interp = interp(energy_interp, energy, intensity)
 
-    # Create the gaussian to model the detector resolution
+    # Create a gaussian to model the detector resolution/source broadening effects
     FWHM = 5
-    mid_point = (min(energy_interp) + max(energy_interp)) / 2
+    mid_point = (min(energy_interp) + max(energy_interp)) / 2  # centre gaussian on spectrum
     y_gauss = gaussian_function(energy_interp, 1, mid_point, FWHM)
     y_convolved = convolve(intensity_interp, y_gauss, mode="same")
 
+    # Plot the results
+    # Raw spectrum
     fig, ax = plt.subplots(2, 1, figsize=(7, 10), sharex=True)
     ax[0].semilogy(energy, intensity, "b-", lw=2, label="FLYCHK")
     ax[0].tick_params(axis="both", labelsize=16, pad=5)
     ax[0].set_ylabel(r"$I$ [10$^{-7}$ J/cm$^{2}$/s/Hz/srad]", fontsize=16)
 
+    # Convolved spectrum
     ax[1].semilogy(energy_interp, y_convolved, "r-", lw=2, label="Convolved (FWHM=" + str(FWHM) + ")")
     ax[1].tick_params(axis="both", labelsize=16, pad=5)
     ax[1].set_xlabel("Energy [eV]", fontsize=16)
     ax[1].set_ylabel(r"$I$ [10$^{-7}$ J/cm$^{2}$/s/Hz/srad]", fontsize=16)
-    ax[1].legend(loc="upper left")
+
+    # Global params/finish up
+    ax[1].legend(loc="upper right")
     ax[1].set_xlim(800, 1500)
     fig.tight_layout()
     plt.show()
