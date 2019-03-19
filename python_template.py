8f525b261f30c15d5d34607af0f5d3f5674b2bdc
Peter Allan <phy3pa@gmail.com>
2019-03-19 20:19:05 +0000

    Base template for python scripts

---
+++ python_template.py
@@ -0,0 +1,21 @@
+from __future__ import print_function, division
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    """
+    Python script template
+
+    Params
+    ------
+
+    Returns
+    -------
+    """
+    pass
+
+
+if __name__ == "__main__":
+
+    main()
