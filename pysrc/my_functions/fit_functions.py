d54a919d882966e0570ea95a2efc472b5d3362a5
Peter Allan <phy3pa@gmail.com>
2019-03-20 08:15:48 +0000

    Added docstring to linear_function. Added a run example to test linear function. Linear function now uses numpy polyval to return result

--- pysrc/my_functions/fit_functions.py
+++ pysrc/my_functions/fit_functions.py
@@ -1,16 +1,45 @@
 from __future__ import division, print_function  # python 2 to 3 compatibility
-from numpy import sqrt, log, exp
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def linear_function(x_array, m, c):
-    return m * x_array + c
+def linear_function(x_array, gradient, intercept):
+    """
+    Linear function of the form y = mx + c
+
+    Parameters
+    ----------
+    x_array (array): array of values to return function over
+    gradient (int/float): gradient
+    intercept (int/float): intercept
+
+    Returns
+    -------
+    ndarray: result of a first order polynomial of the form mx + c
+    """
+    return np.polyval([gradient, intercept], x_array)
 
 
 def gaussian_function(x_array, height, centre, fwhm):
-    c = fwhm / (2 * sqrt(2 * log(2)))
-    return height * exp(-(x_array - centre)**2 / (2.0 * c**2.0))
+    c = fwhm / (2 * np.sqrt(2 * np.log(2)))
+    return height * np.exp(-(x_array - centre)**2 / (2.0 * c**2.0))
 
 
 def sigmoid_function(x_array, minimum, maximum, centre, slope):
-    return minimum + ((maximum - minimum) / (1 + exp((centre - x_array) / slope)))
+    return minimum + ((maximum - minimum) / (1 + np.exp((centre - x_array) / slope)))
 
+
+def run_example(fit_type):
+    if fit_type == "linear":
+        x_array = np.linspace(0, 10)
+        y_array = linear_function(x_array, 2, 5)
+        print(type(y_array))
+        plt.plot(x_array, y_array)
+        plt.show()
+
+
+if __name__ == "__main__":
+
+    # Run example
+    run_example(fit_type="linear")
+    print(linear_function.__doc__)
