fba35c1a05fb81bfac6d9069ea84a1a735e40d49
Peter Allan <phy3pa@gmail.com>
2019-04-02 08:34:50 +0100

    Formatting changes etc.

--- pysrc/creating_lists.py
+++ pysrc/creating_lists.py
@@ -9,86 +9,96 @@ def get_execution_time(start_time):
 
 
 def get_list_properties(input_list):
-    print((sys.getsizeof(input_list)), "bytes", type(input_list), len(input_list))
+    print((sys.getsizeof(input_list)), "bytes", type(input_list))
 
 
 def run_list_creation(elements_in_list):
 
-    # Series of different implementations for creating lists and their performance
+    """
+    Series of different implementations for creating lists and execution times
+    """
 
-    execution_times = []
+    # ------------------------------------------------------------------------------------------------------------
 
-    # First method - simple for loop and append to empty array
+    # 1ST METHOD - for loop and append to empty array
 
     start_time = time.time()
     squares1 = []
-    for i in xrange(1, int(elements_in_list)):
+    for i in range(1, int(elements_in_list)):
         squares1.append(i**2)
 
+    # Get timing info
+    print("Execution times" + "\n--------------------")
     print("Method 1:")
     ex_time1 = get_execution_time(start_time)
     print(ex_time1)
-    execution_times.append(ex_time1)
 
-    # --------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------
 
-    # Second method - list comprehension method
+    # 2ND METHOD - list comprehension
 
     start_time = time.time()
-    squares2 = [i**2 for i in xrange(1, int(elements_in_list))]
+    squares2 = [i**2 for i in range(1, int(elements_in_list))]
 
     print("Method 2:")
     ex_time2 = get_execution_time(start_time)
     print(ex_time2)
-    execution_times.append(ex_time2)
 
-    # --------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------
 
-    # Third method - append to a numpy array
+    # 3RD METHOD - for loop and append to a numpy array
 
     start_time = time.time()
     squares3 = np.zeros(len(squares1), dtype=float)
-    for i in xrange(1, int(elements_in_list) - 1):
+    for i in range(1, int(elements_in_list) - 1):
         squares3[i] = i**2
+
     print("Method 3:")
-    get_execution_time(start_time)
+    ex_time3 = get_execution_time(start_time)
+    print(ex_time3)
 
-    # --------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------
 
-    # Fourth method - use numpy arange
+    # 4TH METHOD - use arange
     # Fastest method
 
     start_time = time.time()
     squares4 = (np.arange(1, int(elements_in_list)))**2
     print("Method 4:")
-    get_execution_time(start_time)
 
-    # --------------------------------------------------------------------------
+    ex_time4 = get_execution_time(start_time)
+    print(ex_time4)
 
-    # Fifth method - use map and lambda function
+    # ------------------------------------------------------------------------------------------------------------
+
+    # 5TH METHOD - use map and lambda function
 
     start_time = time.time()
-    squares5 = map(lambda x: x ** 2, np.arange(1, int(elements_in_list)))
-    print("Method 5:")
-    get_execution_time(start_time)
+    squares5 = list(map(lambda x: x ** 2, np.arange(1, int(elements_in_list))))
 
-    # --------------------------------------------------------------------------
+    print("Method 5:")
+    ex_time5 = get_execution_time(start_time)
+    print(ex_time5)
+    print("\n")
+
+    # ------------------------------------------------------------------------------------------------------------
 
     # Get physical size of arrays
-    print("1.")
+    print("Array sizes and types" + "\n------------------------")
+    print("Method 1:")
     get_list_properties(squares1)
-    print("2.")
+    print("Method 2:")
     get_list_properties(squares2)
-    print("3.")
+    print("Method 3:")
     get_list_properties(squares3)
-    print("4.")
+    print("Method 4:")
     get_list_properties(squares4)
-    print("5.")
+    print("Method 5:")
     get_list_properties(squares5)
 
 
 if __name__ == "__main__":
 
-    elements_in_list = 1e3
+    elements_in_list = 1e6
     run_list_creation(elements_in_list)
 
