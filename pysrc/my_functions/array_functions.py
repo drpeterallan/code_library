1cde6f0d85d222d150a893d7a9999c0be53079f2
Peter Allan <phy3pa@gmail.com>
2019-04-05 08:26:23 +0100

    Added doc strings and included example

--- pysrc/my_functions/array_functions.py
+++ pysrc/my_functions/array_functions.py
@@ -1,7 +1,52 @@
+"""
+----------------------
+Array Functions
+----------------------
+
+Brief description of script
+
+:Date: 05/04/2019
+"""
+
 from __future__ import division, print_function  # python 2 to 3 compatibility
-from numpy import argmin
+from numpy import argmin, array
 
 
 def search_function(array_to_search, search_value):
-    # Return index position of search_value in array_to_search
-    return argmin(abs(array_to_search - search_value))
+
+    """ Function to search an array. Calculates absolute difference between a user given value and each element
+     in the list and returns the index position of the minimum difference, i.e. the closest value
+
+    Parameters
+    ----------
+    array_to_search: list/ndarray
+        list you wish to search
+    search_value: int/float
+        value you wish to find in array
+
+    Returns
+    -------
+    index position: int
+        index position in list of value closest to search_value
+
+    """
+    # Search array and find index position
+    index_position = argmin(abs(array(array_to_search) - search_value))
+
+    return index_position
+
+
+def run_examples():
+
+    # search_function example
+    search_value = 0.73
+    array_to_search = [0, 0.12, 0.56, 0.7, 0.98]
+    index_position = search_function(array_to_search, search_value)
+    print("Index position of value: " + str(index_position))
+    print("Value found: " + str(array_to_search[index_position]))
+
+
+if __name__ == "__main__":
+
+    run_examples()
+
