c556cec77416baa5e8ac18977a81ca4bf2aa6772
Peter Allan <phy3pa@gmail.com>
2019-04-15 07:39:16 +0100

    Added numpy histogram method.

--- pysrc/plotting_examples/histogram.py
+++ pysrc/plotting_examples/histogram.py
@@ -5,20 +5,22 @@ Histogram example
 
 Example of a histogram plot
 
-:Date: """
+:Date: 12/04/19
+"""
 
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 from python_code.pysrc.utils.matplotlibrc_setup import set_rc_params
 
+
 def generate_data():
     """ Generate some random data with a Beta distribution
 
     Returns
     -------
-    Outputs: int/float/list/ndarray
-        description
+    Outputs: ndarray
+        beta distribution values
 
     """
 
@@ -32,9 +34,17 @@ def generate_data():
 if __name__ == "__main__":
 
     # Setup plotting defaults
-    set_rc_params()
+    # set_rc_params()
 
     # Generate and plot data
     data = generate_data()
-    plt.hist(data, bins="auto")
+    plt.hist(data, bins="auto", facecolor="cornflowerblue", edgecolor="black")
     plt.show()
+
+    # Use numpy
+    # y_hist, bin_edges = np.histogram(data, bins="auto", range=None, weights=None, density=None)
+    # print(bin_edges)
+    # plt.bar(bin_edges[1:], y_hist)
+    # print(len(y_hist), len(bin_edges))
+    # # plt.plot(y_hist)
+    # plt.show()
