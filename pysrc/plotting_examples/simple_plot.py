5a55e65ed186a06ae9b431427e8ab659bfdcd70a
Peter Allan <phy3pa@gmail.com>
2019-04-19 06:28:48 +0100

    Initial commit. Simple plot example

--- pysrc/plotting_examples/simple_plot.py
+++ pysrc/plotting_examples/simple_plot.py
@@ -1,33 +1,33 @@
 """
 ----------------------
-Title
+Simple plot
 ----------------------
 
-Brief description of script
+Example of simple data plot
 
-:Date: """
+:Date: 19/04/19
+"""
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-def main():
-    """ Python script template
-
-    Parameters
-    ----------
-    Inputs: int/float/list/ndarray
-        description
-
-    Returns
-    -------
-    Outputs: int/float/list/ndarray
-        description
-
-    """
-
-    return None
+from python_code.pysrc.utils.matplotlibrc_setup import set_rc_params
 
 
 if __name__ == "__main__":
-    main()
+
+    # Generate some data to plot
+    x = np.arange(0, 10)
+    y = x**2
+
+    # Setup plotting
+    set_rc_params()
+    _, ax = plt.subplots(1, 1, figsize=(5, 4))
+
+    # Plot the data
+    ax.plot(x, y, "bo")
+
+    # Finish up
+    ax.set_xlabel("x axis [units]")
+    ax.set_ylabel("y axis [units]")
+    plt.tight_layout()
+    plt.show()
