f7874adc07eb28a464f3df1f26e2f7848da78037
Peter Allan <phy3pa@gmail.com>
2019-04-26 14:19:21 +0100

    Initianl commit. Script to demonstrate using map functions

--- pysrc/map_functions.py
+++ pysrc/map_functions.py
@@ -31,3 +31,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    a = np.linspace(10, 20, 10)
+    b = np.linspace(1, 20, 10)
+
+    lowest = map(min, a, b)
+    print(list(lowest))
+
+    def squared(n):
+        return n * n
+
+    print(list(map(squared, [2, 3, 6])))
+    print(list(map(lambda i: i * i, [2, 3, 6])))
