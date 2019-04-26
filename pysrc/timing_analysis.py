b11c2df68b2e7e26f167b656c802d66eb0567156
Peter Allan <phy3pa@gmail.com>
2019-04-26 14:33:13 +0100

    Initial commit. Timing analysis example

--- pysrc/timing_analysis.py
+++ pysrc/timing_analysis.py
@@ -9,7 +9,13 @@ Brief description of script
 
 import matplotlib.pyplot as plt
 import numpy as np
-
+import datetime as dt
+import time as tm
+print(tm.time())
+dt_now = dt.datetime.now()
+print(dt_now.year, dt_now.month, dt_now.day)
+print(tm.time())
+# Get timing example with formatting
 
 def main():
     """ Python script template
