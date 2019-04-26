860a8afbf6a0e6729ba995411fec4ccfdcaf9e47
Peter Allan <phy3pa@gmail.com>
2019-04-26 14:13:22 +0100

    Gaussian convolution. Alot of formatting changes. Changed plotting. New files: map_functions and timing analysis.

--- pysrc/gaussian_convolution.py
+++ pysrc/gaussian_convolution.py
@@ -3,22 +3,21 @@
 Gaussian Convolution
 ----------------------
 
-Convolve gaussians of different FWHM with a single square pulse and plot results
+Convolve gaussians of different FWHM with a square pulse and plot results
 
 :Date: 05/04/2019
 
 """
 
-from __future__ import division, print_function  # python 2 to 3 compatibility
-from numpy import arange, array, convolve, interp
+import numpy as np
 import matplotlib.pyplot as plt
 from python_code.pysrc.utils.fit_functions import gaussian_function
 from python_code.pysrc.utils.matplotlibrc_setup import set_rc_params
 
 
-def test_mask(width, height, x_array):
+def create_test_mask(width, height, x_array):
 
-    """ Function to create a square pulse and interpolate onto an array
+    """ Function to create a square pulse. This uses numpy 1D linear interpolation
 
     Parameters
     ----------
@@ -27,7 +26,7 @@ def test_mask(width, height, x_array):
     height: int/float
         height of pulse in y axis
     x_array: list/ndarray
-        list/array of x positions to interplate pulse onto
+        list/array of x positions to interpolate pulse onto
 
     Returns
     -------
@@ -36,13 +35,11 @@ def test_mask(width, height, x_array):
     """
 
     # Create a square pulse to convolve with a gaussian
-    x_square = array([2 * -width, -width, -width, width, width, 2 * width], dtype=float)
-    y_square = array([0, 0, height, height, 0, 0], dtype=float)
+    x_square = np.array([2 * -width, -width, -width, width, width, 2 * width], dtype=float)
+    y_square = np.array([0, 0, height, height, 0, 0], dtype=float)
 
-    # Interpolate mask onto gaussian x grid
-    y_array = interp(x_array, x_square, y_square)
-
-    return y_array
+    # Interpolate mask onto x array
+    return np.interp(x_array, x_square, y_square)
 
 
 if __name__ == "__main__":
@@ -51,11 +48,12 @@ if __name__ == "__main__":
     set_rc_params()
 
     # Create a square pulse to convolve with a gaussian
-    x_data = arange(-15, 16, 0.1)
-    y_square = test_mask(5, 10, x_data)
+    x_data = np.arange(-15, 16, 0.1)
+    y_square = create_test_mask(5, 10, x_data)
 
-    # Plot the initial unperturbed data
-    plt.plot(x_data, y_square / max(y_square), "k-", label="Mask")
+    # Setup plotting and plot unperturbed signal
+    _, ax = plt.subplots(1, 1, figsize=(8, 6))
+    ax.plot(x_data, y_square / max(y_square), label="Mask")
 
     # Loop over a range of different gaussian widths
     fwhms = [1, 2, 3, 4, 5]
@@ -65,17 +63,18 @@ if __name__ == "__main__":
         y_gauss = gaussian_function(x_data, 10, 0, fwhm)
 
         # Do the convolution
-        y_convolved = convolve(y_square, y_gauss, mode="same")
+        y_convolved = np.convolve(y_square, y_gauss, mode="same")
 
         # Plot the output
-        plt.plot(x_data + 0.4, y_convolved / max(y_convolved), label=str(fwhm))
+        ax.plot(x_data, y_convolved / max(y_convolved), label=str(fwhm))
 
-        # Finish up plot
-        # plt.tick_params(axis="both", labelsize=16, pad=5)
-        plt.xlabel("Position [a. u.]")
-        plt.ylabel("Signal [a. u.]")
-        plt.xlim(-12, 12)
-        plt.ylim(0, 1.2)
-        plt.legend(title="Gaussian FWHM:")
+        # Setup axes
+        ax.set_xlabel("Position [a. u.]")
+        ax.set_ylabel("Signal [a. u.]")
+        ax.set_xlim(-12, 12)
+        ax.set_ylim(0, 1.2)
 
+    # Finish up
+    plt.tight_layout()
+    plt.legend(title="Gaussian FWHM:", loc="upper right")
     plt.show()
