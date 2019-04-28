3290bb8ec6c33aa149635e1f999c0e45b07d3ee7
Peter Allan <phy3pa@gmail.com>
2019-04-28 08:27:18 +0100

    Changed import and plotting

--- pysrc/utils/matplotlibrc_setup.py
+++ pysrc/utils/matplotlibrc_setup.py
@@ -1,21 +1,21 @@
-import matplotlib as mpl
+from matplotlib import rcParams
 import matplotlib.pyplot as plt
 from numpy import arange
 
 
 def set_rc_params():
 
-    mpl.rcParams.update({"font.size": 14,
-                         "font.family": "sans-serif",
-                         "lines.linewidth": 2,
-                         "xtick.labelsize": 14,
-                         "xtick.major.size": 5,
-                         "xtick.major.width": 1,
-                         "xtick.major.pad": 10,
-                         "ytick.labelsize": 14,
-                         "ytick.major.size": 5,
-                         "ytick.major.width": 1
-                         })
+    rcParams.update({"font.size": 14,
+                     "font.family": "sans-serif",
+                     "lines.linewidth": 2,
+                     "xtick.labelsize": 14,
+                     "xtick.major.size": 5,
+                     "xtick.major.width": 1,
+                     "xtick.major.pad": 10,
+                     "ytick.labelsize": 14,
+                     "ytick.major.size": 5,
+                     "ytick.major.width": 1,
+                     })
 
 
 if __name__ == "__main__":
@@ -23,8 +23,12 @@ if __name__ == "__main__":
     # Set rc params
     set_rc_params()
 
+    # Print all available options
+    print(rcParams.items())
+
     # Plot some data to demo changed rcParams
     x = arange(0, 10)
     y = x * x
-    plt.plot(x, y)
+    _, ax = plt.subplots()
+    ax.plot(x, y)
     plt.show()
