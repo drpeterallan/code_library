8e0bd0e608398c6ef62444e2d9a5755cd029499e
Peter Allan <phy3pa@gmail.com>
2019-08-19 09:02:28 +0100

    Changes to test data produced

--- pysrc/create_test_data.py
+++ pysrc/create_test_data.py
@@ -9,16 +9,16 @@ if __name__ == "__main__":
     """
 
     # Create the data
-    x_data = np.linspace(0, 10, 20)
-    noise_std_dev = 0.5
-    y_data = np.copy(x_data) + np.random.normal(0.0, noise_std_dev, len(x_data))
-    y_data_error = np.random.uniform(1.0, 1.5, len(x_data))
+    x_data = np.linspace(0, 10, 200)
+    noise_std_dev = 5
+    y_data = 10 + np.copy(x_data)**2 + np.random.normal(0.0, noise_std_dev, len(x_data))
+    y_data_error = np.random.uniform(1.0, 5.5, len(x_data))
 
     # Visual check
     plt.errorbar(x_data, y_data, y_data_error, fmt="o")
     plt.show()
 
     # Save data to text file
-    np.savetxt("/Users/pallan/Documents/neutron_bayesian_work/test_fitting_data.txt",
+    np.savetxt("/Users/pallan/Documents/neutron_bayesian_work/test_fitting_data_2nd_order.txt",
                np.transpose([x_data, y_data, y_data_error]),
                fmt="%.3e", delimiter="\t", header="x [a.u.]\ty [a.u.]\ty_error [a.u.]")
