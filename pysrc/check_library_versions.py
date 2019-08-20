e2676124aa8a4905ba4dd2b488924bb58220f5d9
pallan <phy3pa@gmail.com>
2019-08-20 19:22:43 +0100

    Added a version check

--- pysrc/check_library_versions.py
+++ pysrc/check_library_versions.py
@@ -1,6 +1,7 @@
 import pkg_resources
 import numpy
 import theano
+import pymc3
 
 if __name__ == "__main__":
 
@@ -8,3 +9,4 @@ if __name__ == "__main__":
 
     print(numpy.__version__)
     print(theano.__version__)
+    print(pymc3.__version__)
